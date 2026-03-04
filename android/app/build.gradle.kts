0c7008ee79118a4c22950fd6e466bb99f75a7761
unnatii14 <unnatitank14@gmail.com>
2026-03-04 22:23:20 +0530

    Fix package name to com.sleepytales.app and bump version to 1.0.0+2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.sleepytales"
+    namespace = "com.sleepytales.app"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -30,7 +30,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.sleepytales"
+        applicationId = "com.sleepytales.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
